58eac7a542477fb9b716c285c018d9d5f900595b
Karimkrawes21 <abdelkarimboujarra1@gmail.com>
2023-09-08 18:00:44 +0100

    change

--- pages/_app.js
+++ pages/_app.js
@@ -20,6 +20,11 @@ function MyApp({ Component, pageProps }) {
         {/* Your paragraph with inline style */}
         <p style={{ fontWeight: 'bold' }}>Pobierz miniaturę YouTube</p>
 
+        {/* Provided paragraph */}
+        <p>
+          ▷ Zapisuj miniaturki wideo z YouTube
+          Odblokuj możliwość dostępu do darmowych miniatur z dowolnego wideo na YouTube w różnych rozdzielczościach, w tym w pełnym HD (1080), HD (720), SD i mniejszych rozmiarach. Nasza platforma obsługuje szeroki zakres formatów wideo na YouTube, od standardowych po wysokiej jakości (HQ), 1080p, a nawet filmy 4K.
+        </p>
         {/* Rest of your code */}
         <Component {...pageProps} />
       </div>
@@ -28,3 +33,4 @@ function MyApp({ Component, pageProps }) {
 }
 
 export default MyApp;
+
